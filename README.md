270938c9b930f29ff4e350ec87fe881f24a11ae2
thomaspcole <thomascole@carolina.rr.com>
2019-03-23 13:58:54 -0400

    Updated buildAndRun.sh with MacOS support.

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ ISM 452 Team Titan Final Project
 ***
 ## Build Instructions
 
-Make sure docker is installed and you have the ability to install and run images.
+Make sure Docker is installed and you have the ability to install and run images.
 
 ### Windows:
+WIP
 
 ### MacOS:
+Have Docker for MacOS running and run `./buildAndRun.sh`
 
 ### Linux:
 Be a member of the docker group and run `./buildAndRun.sh`
--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,24 +1,39 @@
 #Builds a docker image from the dockerfile and runs the image
-
+#This file works for MacOS and Linux
 #!/bin/bash
-USER=$USERNAME
-GROUP=docker
+function dockerCommands() {
+  echo "Building Image..."
+  docker-compose build
+  echo "Done!"
 
-#Test if user is in the docker group
-if id -nG "$USER" | grep -qw "$GROUP"; then
-    echo $USER belongs to $GROUP
-else
-    echo $USER does not belong to $GROUP
+  echo "Running container. Press CTRL+C to exit"
+  docker-compose up
+
+  docker-compose down
+  echo "Goodbye!"
+}
+
+if [ "$(uname)" == "Darwin" ]; then
+  if [[ $(docker version --format "{{.Server.KernelVersion}}") == *-linuxkit ]]; then
+    echo "Docker is running."
+    dockerCommands
+    exit 0
+  else
+    echo "Docker is not running. Please start docker for mac."
     exit 1
+  fi
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  USER=$USERNAME
+  GROUP=docker
+
+  #Test if user is in the docker group
+  if id -nG "$USER" | grep -qw "$GROUP"; then
+      echo $USER belongs to $GROUP
+  else
+      echo $USER does not belong to $GROUP
+      exit 1
+  fi
+
+  dockerCommands
+  exit 0
 fi
-
-echo "Building Image..."
-docker-compose build
-echo "Done!"
-
-echo "Running container. Press CTRL+C to exit"
-docker-compose up
-
-docker-compose down
-echo "Goodbye!"
-exit 0
